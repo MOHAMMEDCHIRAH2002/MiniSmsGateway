45ae541573ba24c1d4321e4c2e1d61a3c6ec141c
mohammed <mohammed.chirah@gmail.com>
2025-04-25 12:09:52 +0100

    definition_Config_in_ClassSmsConfig

---
+++ MiniSmsGateway/SmsConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSmsGateway
+{
+    internal class SmsConfig
+    {
+        public string SMSApiUrl { get; set; } = "";
+        public string ApiKey { get; set; } = "";
+        
+        public int HttpPort { get; set; } 
+
+        public int RetryCount { get; set; } = 3;
+
+        public int TimeoutMs { get; set; } = 3000;
+        public bool MockEnabled { get; set; } = true;
+
+
+    }
+}
