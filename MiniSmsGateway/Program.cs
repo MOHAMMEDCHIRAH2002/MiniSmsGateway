6606bbbcde3d23580460e9621a3b57ab9ae156fc
mohammed <mohammed.chirah@gmail.com>
2025-04-25 12:17:55 +0100

    Relate_ConfigSms_with_SmsConfig_Class

--- MiniSmsGateway/Program.cs
+++ MiniSmsGateway/Program.cs
@@ -1,6 +1,11 @@
 using MiniSmsGateway;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+
+builder.Services.Configure<SmsConfig>(
+builder.Configuration.GetSection("SmsGateway"));
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
